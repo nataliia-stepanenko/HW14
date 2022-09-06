b29891ff9b848bb54a850ba2ae7663677e03740f
Natali <C:\Users\Oleksandr\AppData\Roaming\The Bat!>
2022-09-06 17:17:27 +0300

    Added quantifiers to a regular expression in task 2

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@
 
 // 2
 // function isValidDateFormat(str) {
-//     let regexp = /^\d\d\d\d\/\d\d\/\d\d \d\d\:\d\d$/;
+//     let regexp = /^\d{4}\/\d{2}\/\d{2} \d{2}\:\d{2}$/;
 //     return regexp.test(str)
 // }
 
