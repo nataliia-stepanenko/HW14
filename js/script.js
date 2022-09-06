d8bc412ee8a414561fc6599e0c11448629c5e9af
Natali <C:\Users\Oleksandr\AppData\Roaming\The Bat!>
2022-09-06 17:50:58 +0300

    Task 3: deleted regular expression check at the beginning of function

--- js/script.js
+++ js/script.js
@@ -24,15 +24,8 @@
 
 
 // 3
-// function isAfter(date1, date2) {
-//     let regexp = /^\d\d\d\d\d\d\d\d\d\d\d\d\d$/;
-//     if (!regexp.test(date1)) {
-//         date1 = Date.parse(date1)
-//     } 
-//     if (!regexp.test(date2)) {
-//         date2 = Date.parse(date2)
-//     }
-//     return date1 > date2;
+// function isAfter(date, dateToCompare) {
+//     return date > dateToCompare;
 // }
 
 // console.log(isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11))); // true
